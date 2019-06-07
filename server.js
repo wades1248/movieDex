08f8668e431d9cc5c0e3f18cfa70377c889040a5
Spencer Wade <wades1248@gmail.com>
2019-06-07 17:45:07 -0500

    fixed listening error

--- server.js
+++ server.js
@@ -49,14 +49,18 @@ app.get('/movie', function handleGetMovies(req, res) {
 })
 
 app.use((error, req, res, next) => {
-    let response
-    if (process.env.NODE_ENV === 'production') {
-      response = { error: { message: 'server error' }}
-    } else {
-      response = { error }
-    }
-    res.status(500).json(response)
-  })
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
 
 const PORT = process.env.PORT || 8000;
+
+app.listen(PORT,() => {
+    console.log(`Server listening at http://localhost:${PORT}`)
+})
 
